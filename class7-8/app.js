4eef72f8c305edefe65ae1ac8b94d9ebd3c4c920
GabrielBRQ <gabrielmedvalle@gmail.com>
2026-03-26 12:59:44 -0300

    add class9

--- class7-8/app.js
+++ class7-8/app.js
@@ -1,33 +1,45 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('../models/blog');
+require('dotenv').config();
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI = process.env.MONGO_URI;
+
+mongoose.connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
-
 // middleware & static files
 app.use(express.static('public'));
-
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     //res.send('<p>home page</p>');
-    const blogs = [
-        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-    ];
-    res.render('index', { title: 'Home', blogs })
+    res.redirect('/blogs');
 })
 
 app.get('/about', (req, res) => {
     //res.send('<p>about page</p>');
     res.render('about', { title: 'About' })
+})
+
+//blog routes
+app.get('/blogs', (req,res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 })
 
 app.get('/blogs/create', (req, res) => {
